007e63b95649503741011e35a29fe54cb40e637c
Steven Karas <steven.karas@gmail.com>
2016-03-05 13:02:32 +0200

    simple cli interface for working with Kasefet

---
+++ lib/kasefet/cli.rb
@@ -0,0 +1,52 @@
+require "thunder"
+
+require "kasefet/config"
+
+class Kasefet
+  class CLI
+    GlobalConfigLocations = [
+      "~/.kasefet",
+      "~/.config/kasefet",
+    ]
+
+    DefaultWalletLocation = "~/.wallet"
+
+    include Thunder
+
+    def load_config(options)
+      config_file = options[:config]
+      config_file ||= GlobalConfigLocations.find { |file| File.exist?(File.expand_path(file)) }
+      config_file ||= File.expand_path(GlobalConfigLocations.first)
+      @config = Kasefet::Config.new(config_file)
+    end
+
+    def load_wallet
+      wallet_dir = @config["wallet"]
+      wallet_dir = @config["wallet"] = DefaultWalletLocation unless wallet_dir
+      @wallet = Kasefet::Wallet.new(directory: wallet_dir)
+    end
+
+    desc "add KEYNAME CONTENT..."
+    def add(keyname, *content, **options)
+      load_config(options)
+      load_wallet
+
+      content = content.join(" ")
+
+      @wallet.store_credentials(keyname, content)
+
+      return content
+    end
+
+    desc "show KEYNAME"
+    def show(keyname, **options)
+      load_config(options)
+      load_wallet
+
+      content = @wallet.read_credentials(keyname)
+
+      puts content
+      return content
+    end
+  end
+end
---
+++ test/cli_test.rb
@@ -0,0 +1,42 @@
+require File.expand_path("test_helper", __dir__)
+
+require "kasefet/config"
+require "kasefet/cli"
+require "kasefet/wallet"
+require "fileutils"
+
+class CLITest < Minitest::Test
+  def setup
+    @tmpdir = Dir.mktmpdir
+    @config = Kasefet::Config.new(@tmpdir + "kasefet.json")
+    @config["wallet"] = @tmpdir + "wallet"
+    @config.save
+    @cli = Kasefet::CLI.new
+  end
+
+  def teardown
+    FileUtils.remove_entry(@tmpdir)
+  end
+
+  def test_loads_config_from_option
+    config = @cli.load_config(config: @config.file)
+    assert_equal config.file, @config.file
+    assert_equal config["wallet"], @config["wallet"]
+  end
+
+  def test_creates_wallet
+    @cli.load_config(config: @config.file)
+    wallet = @cli.load_wallet
+    assert_equal wallet.root.to_s, @config["wallet"]
+  end
+
+  def test_stores_and_prints_a_password
+    saved_content = @cli.add("foo", "bar", "baz", config: @config.file)
+    assert_equal "bar baz", saved_content
+
+    assert_output(/^bar baz$/) do
+      loaded_content = @cli.show("foo", config: @config.file)
+      assert_equal "bar baz", loaded_content
+    end
+  end
+end
